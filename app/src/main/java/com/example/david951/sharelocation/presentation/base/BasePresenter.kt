7566b0baaa7e9d8e29e0bd8b024e4abe85af5b3b
Devit951 <Devit951@gmail.com>
2018-07-20 16:58:04 +0500

    Added play services location

--- app/src/main/java/com/example/david951/sharelocation/presentation/base/BasePresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.david951.sharelocation.base
-
-open class BasePresenter<View : BaseView> {
-    var view: View? = null
-
-    fun detachView() { view = null }
-}
--- app/src/main/java/com/example/david951/sharelocation/presentation/base/BaseView.kt
+++
@@ -1,3 +0,0 @@
-package com.example.david951.sharelocation.base
-
-interface BaseView
